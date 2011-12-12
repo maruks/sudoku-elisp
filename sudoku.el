e399cec2f82d69c60519cff095b5e9d901ebb87b
Maris Orbidans <maris.orbidans@gmail.com>
2011-12-12 19:52:27 +0000

    added some functions

--- sudoku.el
+++ sudoku.el
@@ -4,7 +4,8 @@
 
 
 (defun range (from to &optional step) 
-  (let ((f from) (res '()))
+  (let ((f from) 
+	(res '()))
     (progn 
       (while (< f to) 
 	(setq 
@@ -12,23 +13,33 @@
 	 f (+ f (if (null step) 1 step)))) 
       (reverse res))))
 
-(defun set-union ())
+(defun set-union (first &rest rest) (delete-dups (append first (apply 'append rest))))
+
 (defun set-diff ())
 
+(defun select-row (vek i) (let* ((start (- i (% i 9))) 
+				 (end (+ start 9))) 
+			    (delete-dups (mapcar (lambda (i) (elt vek i)) (range start end)))))
 
-(defun select-row (vek index))
-(defun select-column (vek index))
-(defun select-square (vek index))
+(defun select-column (vek i) (let* ((start (% i 9)) 
+				    (end (+ start 73))) 
+			       (delete-dups (mapcar (lambda (i) (elt vek i)) (range start end 9)))))
+
+(defun select-square (vek i) (let* ((x (- (% i 9)(% (% i 9) 3))) 
+				    (y (- (/ i 9) (% (/ i 9) 3))) 
+				    (s (+ x (* y 9))) 
+				    (r (range s (+ s 3))) 
+				    (rng (set-union r (mapcar (lambda (x) (+ x 9)) r) (mapcar (lambda (x) (+ x 18)) r)))) 
+			       (delete-dups (mapcar (lambda (i) (elt vek i)) rng))))
 
 (defun read-sudoku (vek buffer) 
   (if (< (length vek) 81) 
       (read-sudoku (cons (read buffer) vek) buffer) (coerce (reverse vek) 'vector)))
 
-(defun solve-buffer (buffer) (let   
-	   ((vek (read-sudoku '() buffer)))
-	   (length vek)))
+(defun solve-buffer (buffer) 
+  (read-sudoku '() buffer))
 
-(defun solve () (solve-buffer (get-buffer "board")))
+(defun solve () (solve-buffer (get-buffer "puzzle4.txt")))
 
 
 
